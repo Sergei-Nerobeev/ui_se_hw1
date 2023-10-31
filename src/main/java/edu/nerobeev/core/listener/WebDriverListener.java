d4f23a9dc75243d76ce81d953c528bfcc83383d4
Sergei Nerobeev <sergeinerobeev@gmail.com>
2023-10-31 22:36:48 +0100

    Listener

---
+++ src/main/java/edu/nerobeev/core/listener/WebDriverListener.java
@@ -0,0 +1,7 @@
+package edu.nerobeev.core.listener;
+
+import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
+
+public class WebDriverListener extends AbstractWebDriverEventListener {
+
+}
--- src/main/java/edu/nerobeev/core/ui/browser/WebDriverFactory.java
+++ src/main/java/edu/nerobeev/core/ui/browser/WebDriverFactory.java
@@ -2,29 +2,32 @@ package edu.nerobeev.core.ui.browser;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.lang3.NotImplementedException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
-import java.time.Duration;
 import static edu.nerobeev.config.GlobalParameter.BROWSER;
-import static edu.nerobeev.config.GlobalParameter.TIMEOUT;
+
 
 public final class WebDriverFactory {
 
 	public static WebDriver getWebDriver(){
 		WebDriver webDriver;
+		Logger logger = LogManager.getLogger();
+		logger.info("Starting webDriver..");
 		webDriver= switch (BROWSER) {
-			case CHROME -> {
+			case BrowserType.CHROME -> {
 				WebDriverManager.chromedriver().setup();
 				yield new ChromeDriver();
 			}
-			case FIREFOX -> {
+			case BrowserType.FIREFOX -> {
 				WebDriverManager.firefoxdriver().setup();
 				yield new FirefoxDriver();
 			}
-			case SAFARI -> {
+			case BrowserType.SAFARI -> {
 				WebDriverManager.safaridriver().setup();
 				yield new SafariDriver();
 			}
